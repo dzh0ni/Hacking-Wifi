eb3b3fd8ac7da63fa56992b27136ec4fd977a7ad
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-10-23 04:54:13 -0300

    menu.sh

--- menu.sh
+++ menu.sh
@@ -72,7 +72,7 @@ fun_banner() {
 }
 
 # Función de actualización
-P_SERVER="nt.com/AAAAAEXQOSyIpN2JZ0ehUQ/Hacking-Wifi/refs/heads/main/Install/"
+P_SERVER="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/Hacking-Wifi/refs/heads/main/Install/"
 v1=$(curl -sSL "${P_SERVER}/versionact")
 v2=$(cat /opt/Hacking-Wifi/Install/versionact)
 
