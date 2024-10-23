ed387ffa0bb146960b4daac587293f70df82238c
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-10-23 03:54:41 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd /opt
 sudo rm -rf Hacking-Wifi
-sudo git clone git@github.com:AAAAAEXQOSyIpN2JZ0ehUQ/Hacking-Wifi.git
+sudo git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/Hacking-Wifi.git
 sudo chmod +x Hacking-Wifi/*
 cd Hacking-Wifi
 ls -ltha
