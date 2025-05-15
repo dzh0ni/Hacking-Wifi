18e465583a52367b3f2ae4e2786425b6a026f2a1
Jony Rivera <jrsepulveda1988@gmail.com>
2025-05-15 01:21:06 -0400

    Update Script

--- install.sh
+++ install.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 cd /opt
-sudo rm -rf Hacking-Wifi
-sudo git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/Hacking-Wifi.git
-sudo chmod +x Hacking-Wifi/*
-cd Hacking-Wifi
-ls -ltha
-cd 
-sudo echo "cd /opt/Hacking-Wifi && sudo ./menu.sh" > hackingwifi
-sudo chmod +x hackingwifi
-sudo mv hackingwifi /usr/local/bin/
+sudo rm -rf evilTrust
+sudo git clone https://github.com/s4vitar/evilTrust.git
+cd evilTrust
+sudo chmod +x evilTrust.sh
 cd
+
+cd
+sudo echo "cd && cd /opt/evilTrust && sudo ./evilTrust.sh -m terminal" > eviltrust
+sudo echo "cd && cd /opt/evilTrust && sudo ./evilTrust.sh -m gui" > eviltrustg
+sudo chmod +x eviltrust
+sudo chmod +x eviltrustg
+sudo rm -rf /usr/local/bin/eviltrust
+sudo rm -rf /usr/local/bin/eviltrustg
+sudo mv eviltrust /usr/local/bin/
+sudo mv eviltrustg /usr/local/bin/
+cd
+
+sudo apt-get -y update
+sudo apt-get install -y php
+sudo apt-get install -y dnsmasq
+sudo apt-get install -y hostapd
+
+sudo mkdir -p /usr/local/share/evilTrust/images
+sudo cp images/evil.png /usr/local/share/evilTrust/images/
+sudo chmod 644 /usr/local/share/evilTrust/images/evil.png
+
+sudo wget https://raw.githubusercontent.com/dzh0ni/evilTrust/master/Install/crear_evilTrust_desktop.sh -O - | sudo bash && sudo rm -rf wget-log*
